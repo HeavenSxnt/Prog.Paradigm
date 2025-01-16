f1549494b52d3e2b8940638a8826afe0cf9edb3f
HeavenSxnt <siriwat.ispb@gmail.com>
2025-01-16 16:54:35 +0000

    update

--- Ex2/Main.java
+++ Ex2/Main.java
@@ -1,13 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main{
 
     public static void main(String[] args){
+        // Declaration
+        int threshold;
         Scanner input = new Scanner(System.in);
-        //Scanner FileScan = new Scanner(new File("/workspaces/Prog.Paradigm/Ex2/platforms.txt"));
-        System.out.println("Read platform data from")
-        int thres;
+        File FileScan = new File("platforms.txt");
+        File Output = new File("output.txt");
+
+        // Location of platforms.txt
+        try{
+            System.out.println("Read platform data from " + FileScan.getAbsolutePath());
+        } catch (Exception e) {System.err.println(e);}
+
+        // Scan threshold
         System.out.println("Enter MAU threshold in millions = ");
-        thres = input.nextInt();
+        threshold = input.nextInt();
+
+        // Location of output.txt
+        try{
+            System.out.println("Write output " + Output.getAbsolutePath());
+        } catch (Exception e) {System.err.println(e);}
+
+        // Write on output.txt
+        try {
+            Scanner Original = new Scanner(FileScan);
+            PrintWriter WriteOutput = new PrintWriter(Output);
+            WriteOutput.printf("%s %s %s >%d millions\r\n","Platform", "MAU(thousands)", "YAU(billions)", threshold);
+        } catch (Exception e) {System.err.println(e);}
     }
 }
