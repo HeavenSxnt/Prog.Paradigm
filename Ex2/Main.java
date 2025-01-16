dbcce6feb73ef4f12638d2578b10c4bc58fe6bef
HeavenSxnt <siriwat.ispb@gmail.com>
2025-01-16 18:18:24 +0000

    update

--- Ex2/Main.java
+++ Ex2/Main.java
@@ -6,6 +6,7 @@ public class Main{
     public static void main(String[] args){
         // Declaration
         int threshold;
+        String thresCheck;
         Scanner input = new Scanner(System.in);
         File FileScan = new File("platforms.txt");
         File Output = new File("output.txt");
@@ -18,6 +19,7 @@ public class Main{
         // Scan threshold
         System.out.println("Enter MAU threshold in millions = ");
         threshold = input.nextInt();
+        input.close();
 
         // Location of output.txt
         try{
@@ -28,7 +30,22 @@ public class Main{
         try {
             Scanner Original = new Scanner(FileScan);
             PrintWriter WriteOutput = new PrintWriter(Output);
-            WriteOutput.printf("%s %s %s >%d millions\r\n","Platform", "MAU(thousands)", "YAU(billions)", threshold);
+            WriteOutput.printf("%-15s %-20s %-17s >%,d millions\r\n","Platform", "MAU(thousands)", "YAU(billions)", threshold);
+            WriteOutput.printf("=".repeat(80));
+            WriteOutput.printf("\r\n");
+            while(Original.hasNext()){
+                String platform = Original.next();
+                int num = Original.nextInt();
+                int MAU = num*1000;
+                float YAU = (float)num/1000;
+
+                if(num > threshold) {thresCheck = "yes";}
+                else {thresCheck = "no";}
+                WriteOutput.printf("%-15s %,12d %18.3f %15s\r\n",platform, MAU, YAU, thresCheck);
+            }
+            WriteOutput.flush();
+            WriteOutput.close();
+            Original.close();
         } catch (Exception e) {System.err.println(e);}
     }
 }
