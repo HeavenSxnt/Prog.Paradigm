882ec89c26ae5241ca797e6dcaacd88d074312d7
HeavenSxnt <siriwat.ispb@gmail.com>
2025-01-16 08:37:19 +0000

    update

--- Ex2/Main.java
+++ Ex2/Main.java
@@ -4,6 +4,8 @@ public class Main{
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        //Scanner FileScan = new Scanner(new File("/workspaces/Prog.Paradigm/Ex2/platforms.txt"));
+        System.out.println("Read platform data from")
         int thres;
         System.out.println("Enter MAU threshold in millions = ");
         thres = input.nextInt();
