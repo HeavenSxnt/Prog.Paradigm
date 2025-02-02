386e265809a3ef3ec4ee0a83331048b2c8195056
HeavenSxnt <siriwat.ispb@gmail.com>
2025-02-02 20:50:46 +0000

    update

--- Ex5/Main.java
+++ Ex5/Main.java
@@ -5,6 +5,18 @@
 import java.util.*;
 import java.io.*;
 
+class InvalidYearException extends Exception {
+    public InvalidYearException(String message) {
+        super(message);
+    }
+}
+
+class InvalidNumberException extends Exception {
+    public InvalidNumberException(String message) {
+        super(message);
+    }
+}
+
 class Company implements Comparable<Company>{
     private String name;
     private int year, marketValue, profit, sales;
@@ -17,7 +29,11 @@ class Company implements Comparable<Company>{
         this.sales = sales;
     }
 
-    // Compare to decreasing order
+    public void printInfo(){
+        System.out.printf("%-22s (%d) %,17d %17d %15d\n",name, year, marketValue, profit, sales);
+    }
+
+    @Override
     public int compareTo(Company other){
         if(this.marketValue > other.marketValue){return -1;}
         else if(this.marketValue < other.marketValue){return 1;}
@@ -34,66 +50,79 @@ class Company implements Comparable<Company>{
         else{return 0;} 
     }
 
-    public void printInfo(){
-        System.out.printf("%-22s (%d) %,17d %17d %15d\n",name, year, marketValue, profit, sales);
+    @Override
+    public String toString(){
+        return name + ":" + year + ":" + marketValue + ":" + profit + ":" + sales;
     }
 }
 
 public class Main{
+    public static void CheckingYear(int year) throws InvalidYearException {
+        if (year < 0 || year > 2024) throw new InvalidYearException("For year: \"" + year + "\"");
+    }
+
+    public static void PositiveNum(int[] number) throws InvalidNumberException {
+        for (int i=0; i<number.length; i++) {
+            if (number[i] < 0) throw new InvalidNumberException("For input: \"" + number[i] + "\"");
+        }
+    }
     public static void main(String[] args){
-        File FileInput = new File("companies.txt");
+        Scanner Scan = new Scanner(System.in);
+        File FileInput = new File("companies_error.txt");
         // File FileInput = new File("src/main/java/Ex4_6581098/companies.txt");
-        try{
-            while(true){
-                // Declaration
-                ArrayList<Company> Companies = new ArrayList<Company>();
-                Scanner FileScan = new Scanner(FileInput);
+        ArrayList<Company> Companies = new ArrayList<Company>();
+
+        while(true){
+            try(Scanner FileScan = new Scanner(FileInput)){
                 FileScan.nextLine();
-                Scanner Scan = new Scanner(System.in);
-                
-                // Header & non-numeric value
-                int threshold;
-                System.out.println("Enter year threshold = ");
-                while(true){
-                    try{
-                        threshold = Scan.nextInt();
-                        break;
-                    }catch(InputMismatchException e){
-                        Scan.nextLine();
-                        System.err.println(e + ": Invalid Input");
-                    }
-                }
-                System.out.printf("Company established  since %d    Market Value($Bn.).    Profit($Bn.)    Sales($Bn.)\n",threshold);
-                System.out.println("=".repeat(90));
 
-                // Skip the 1st line & get info
-                for(int i=0; i<=27; i++){
+                while(FileScan.hasNext()){
                     String line = FileScan.nextLine();
-                    String [] cols = line.split(",");
-                    String name = cols[0].trim();
-                    int year = Integer.parseInt(cols[1].trim());
-                    int marketValue = Integer.parseInt(cols[2].trim());
-                    int profit = Integer.parseInt(cols[3].trim());
-                    int sales = Integer.parseInt(cols[4].trim());
-
-                    if(year>= threshold){
+                    try{
+                        String [] cols = line.trim().split("\\s*,\\s*");
+                        String name = cols[0];
+                        int year = Integer.parseInt(cols[1]);
+                        int marketValue = (int) Double.parseDouble(cols[2]);
+                        int profit = (int) Double.parseDouble(cols[3]);
+                        int sales = (int) Double.parseDouble(cols[4]);
+                        CheckingYear(year);
+                        int[] number = {marketValue, profit, sales};
+                        PositiveNum(number);
                         Companies.add(new Company(name, year, marketValue, profit, sales));
+                    }catch(Exception e){
+                        System.err.println(e);
+                        System.out.println(line + "\n");
                     }
-                }
-
-                // Print all
-                Collections.sort(Companies);
-                for(int i=0; i<Companies.size(); i++){
-                    Companies.get(i).printInfo();
                 }
                 FileScan.close();
-
-                // Continue or break 
-                System.out.println("\n\n\nEnter y or Y to continue, else to quit = ");
-                String Choice = Scan.nextLine();
-                if (Choice.equals("y")||Choice.equals("Y")) {continue;}
-                else {Scan.close(); break;}
+                Collections.sort(Companies);
+                break;
+            }catch (FileNotFoundException e){
+                System.err.println(e);
+                System.out.println("New File Name = ");
+                FileInput = new File(Scan.nextLine().trim());   
+                // FileInput = new File("src/main/Java/Ex5_6581098/" + Scan.nextLine().trim());  
             }
-        }catch (Exception e){ System.err.println(e);}
+        }
+        
+        // Scanner
+        int threshold;
+        System.out.println("Enter year threshold = ");
+        while(true){
+            try{
+                threshold = Scan.nextInt();
+                break;
+            }catch(InputMismatchException e){
+                Scan.nextLine();
+                System.err.println(e + ": Invalid Input");
+            }
+        }
+
+        System.out.printf("Company established  since %d    Market Value($Bn.).    Profit($Bn.)    Sales($Bn.)\n",threshold);
+        System.out.println("=".repeat(90));
+        for(int i=0; i<Companies.size(); i++){
+            Companies.get(i).printInfo();
+        }
+        Scan.close();
     }
 }
