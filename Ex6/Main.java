2ec8fd8c04c18d5d0538bccdbf011eb237b7f2e6
HeavenSxnt <siriwat.ispb@gmail.com>
2025-02-18 07:51:37 +0000

    (1) start

--- Ex6/Main.java
+++ Ex6/Main.java
@@ -5,7 +5,29 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+class OneCard {
+    private int score; // 0-51
+    private int suit; // clubs, diamonds, hearts, spades
+    private int rank; // ace, 2-10, jack, queen, king   
+    public OneCard(int sc) { score = sc; }  
+}
+
+class CardThread extends Thread {
+    private PrintWriter out;
+    private ArrayList<OneCard> randomCards;
+    public void run() {
+    // Create PrintWriter object to write result to a separate file
+    // Execute steps 1-3 in loop:
+    // 1. Draw 4 cards from the same deck. The cards must not duplicate.
+    // 2. Print round number and these 4 cards to output file.
+    // 3. If all cards are from the same suit or have equal rank, stop the loop.
+    // Otherwise, clear randomCards & continue to the next round.
+    // After the loop, print #rounds to the screeen
+}
+
+
+
+}public class Main{
     public static void main(String[] args){
         
     }
