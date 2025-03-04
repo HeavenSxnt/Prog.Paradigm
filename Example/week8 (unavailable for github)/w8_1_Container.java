b8d6ca331e4028536a748416a8119a1dd78588ed
HeavenSxnt <siriwat.ispb@gmail.com>
2025-03-04 07:25:29 +0000

    update

--- Example/week8 (unavailable for github)/w8_1_Container.java
+++ Example/week8 (unavailable for github)/w8_1_Container.java
@@ -1,4 +1,4 @@
-package Lab_Ch8;
+//package Lab_Ch8;
 
 import java.awt.*;
 import javax.swing.*;
@@ -17,7 +17,7 @@ class w8_1_Container
 	// ----- (2) Other properties
 	//           - which one doesn't work ?
 	//           - notice the difference between EXIT_ON_CLOSE and DISPOSE_ON_CLOSE
-	/*
+	
         frame.setMaximizedBounds( new Rectangle(800, 500) );
 	frame.setResizable(true);
 	frame.setBackground( new Color(100, 150, 250) );
@@ -27,7 +27,7 @@ class w8_1_Container
         
         frame.getContentPane().setLayout(new FlowLayout());
         frame.getContentPane().add( new JTextField("type anything", 20) );
-        */
+        
 
         
         // ----- (1.1) If the frame is visible before components are added,
@@ -44,19 +44,19 @@ class w8_1_Container
 	// ----- (3) Dialog
 	//           - notice the difference between dialog with and without owner
 	//           - notice the difference between modal and non-modal dialogs
-	/*
+	
 	JDialog dialog = new JDialog();
 	dialog.setTitle("This is a Dialog");
 	dialog.setModal(false);
 	dialog.setBounds(400, 400, 300, 150 );
 	dialog.setVisible(true);
 	dialog.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
-	*/
+	
 
 
 	// ----- (4) Quick dialog 
         //           - notice the difference between dialog with and without owner
-	/*
+	
 	int op = JOptionPane.showConfirmDialog( null, 
 		 "Press the button", "This is a Confirm Dialog", 
 	         JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE );
@@ -64,6 +64,6 @@ class w8_1_Container
 	JOptionPane.showMessageDialog( frame, 
 		 ("Option  =  " + op), "This is a Message Dialog", 
 		 JOptionPane.INFORMATION_MESSAGE );
-	*/
+	
     }
 }
